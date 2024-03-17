57ab7799b330b8321c59ace199060cc813f29a99
Azamatjon <azamatergasev01@gmail.com>
2024-03-18 02:03:33 +0500

    js tasks

---
+++ js tasks/main.js
@@ -0,0 +1,96 @@
+
+
+//  1 task starts 
+
+// let number = prompt(" Enter your number ...")
+
+// let juft = 0 
+// let toq = 0 
+
+// let result = 0 
+
+// for (let i = 0 ; i <= number ; i++) {
+//     if ( i % 2 == 0) {
+
+//         juft = juft + i 
+
+
+//     }else{
+//         toq = toq + i 
+//     }
+
+
+// }
+// Math.abs()
+// result = Math.abs(juft - toq)
+
+// console.log(`Siz kiritgan juft son bilan toq sonlar orasidagi farq ${result} ga teng`) 
+
+
+
+// 1 task finished 
+
+
+// 2 task starts 
+
+// let number1 = +prompt(" Enter your 1 number ...")
+
+// let number2 = +prompt("Enter your 2 number ...")
+
+
+// if (number1 < number2) {
+
+//     for ( let i = number1 ; i < number2 ; i++) {
+
+//         console.log(i)
+
+//     }
+
+// }else{
+//     for ( let i = number2 ; i < number1 ; i++) {
+
+//         console.log(i)
+
+//     }
+// }
+
+// task 2 finished 
+
+
+// task 3 starts 
+
+
+// let number = +prompt(" Enter your number ... ")
+
+// juft = 0
+
+// if (number % 2 == 0) {
+
+//    for ( let i = 1 ; i <= number ; i++) {
+
+//     juft = juft ++
+//     console.log(i) 
+
+//    }
+
+// }
+
+// console.log(juft)
+
+
+//  4 task start 
+// let string = "Azamatjon " 
+
+// let count = 0 
+
+// for(let i = 0; i< string.length ; i++) {
+
+//     count++
+
+// }
+
+// console.log(count)
+
+
+//  4 finish 
+
